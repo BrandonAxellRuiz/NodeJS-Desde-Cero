912918b2e27e8ae6674f0c7eee1579eb78fa5c84
Brandon Axell Ruiz <brandon_axell@hotmail.com>
2019-03-18 23:21:15 -0600

    add color const req

---
+++ 04-to-do/index.js
@@ -0,0 +1,25 @@
+const argv = require('yargs').argv;
+const colors = require('colors/safe');
+
+
+let comando = argv._[0];
+
+switch (comando){
+
+    case 'Crete':
+
+    break;
+
+    case 'List':
+
+    break;
+
+    case 'Actualizar':
+
+    break;
+
+
+    default:
+
+    break;
+}
